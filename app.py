03c9d025d7f2b61cdf2ea95b4eaf9f8c8f8d3b46
BhagatTushar <114748864+BhagatTushar@users.noreply.github.com>
2025-06-07 21:00:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def predict_audio():
     return jsonify({"prediction": result, "confidence": round(float(confidence), 2)})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+        app.run(debug=True)
